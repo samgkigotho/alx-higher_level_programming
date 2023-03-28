bbacb0117f2082fb567c71908b9547a3a2fe6cb9
samgkigotho <samgatambia@gmail.com>
2023-03-27 18:46:00 -0700

    Python Exceptions

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    int = "9"
+    print(9 + int)
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    try:
+        x = hello
+    except NameError:
+        print(message)
